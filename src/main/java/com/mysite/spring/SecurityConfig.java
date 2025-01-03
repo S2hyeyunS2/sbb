d03915e4ff77316a177790b14ecd3e6b014b0cb0
S2hyeyunS2 <hhyeyun22@gmail.com>
2025-01-03 11:51:22 +0900

    3-5

--- src/main/java/com/mysite/spring/SecurityConfig.java
+++ src/main/java/com/mysite/spring/SecurityConfig.java
@@ -1,4 +1,27 @@
 package com.mysite.spring;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration //스프링의 환경 설정 파일이다
+@EnableWebSecurity //모든 요청 URL이 스프링 시큐리티의 제어를 받도록 만듦
 public class SecurityConfig {
+    @Bean //스프링에 의해 생성 또는 관리되는 객체
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+        ;
+        return http.build();
+    }
 }
